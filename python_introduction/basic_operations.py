e92071abde0b3ec49b3c906f81f98e6cec8f1a23
Bryan Mukera <bryanmukera4@gmail.com>
2025-07-13 00:57:19 +0300

    Conducted basic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+subtraction = number1 - number2
+product = number1 * number2
+print("Sum of", number1, "and", number2, "is", sum)
+print("Difference of", number1, "and", number2, "is", subtraction)
+print("Product of", number1, "and", number2, "is", product)
