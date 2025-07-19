f2dfeb04c57b25bd15805e681776b6d04d2da104
Bryan Mukera <bryanmukera4@gmail.com>
2025-07-19 11:26:27 +0300

    Added a counter to the number guessing game.

--- python_introduction/match_case_game.py
+++ python_introduction/match_case_game.py
@@ -3,19 +3,24 @@ import random
 
 while True:
   secret_number = random.randint(1,10)
-  guess = int(input("Guess a number between 1 and 10: "))
+  guess_count = 0
+  
+  while True:
+    guess = int(input("Guess a number between 1 and 10: "))
+    guess_count += 1
 
-
-  match (guess > secret_number, guess < secret_number):
-    case (False, False):   # guess == secret_number
-      print("Congratulations, you guessed it!")
-    case (True, False):
-      print("Oops, your guess is a bit high. Try again!")
-    case (False, True):
-      print("Nope, your guess is abit low. Give it another shot!")
-  continuation = input("Do you want to play again (Yes or No):")
-  if continuation == "Yes":
+    match (guess > secret_number, guess < secret_number):
+      case (False, False):   # guess == secret_number
+        print(f"Congratulations, you guessed it {guess_count} attempt(s)!")
+        break
+      case (True, False):
+        print("Oops, your guess is a bit high. Try again!")
+      case (False, True):
+        print("Nope, your guess is abit low. Give it another shot!")
+  continuation = input("Play again? (yes/no):")
+  if continuation == "yes":
     print("You can play again.")
   else:
     print("ThankYou for playing.")
     break
+
