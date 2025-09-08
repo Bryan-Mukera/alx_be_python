310cc9ab5188ba9a84cf2ce632196309cc97a6ba
Bryan Mukera <bryanmukera4@gmail.com>
2025-09-08 13:56:38 +0300

    Importing functions

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,16 @@
+def perform_operation(num1, num2, operation):
+  if operation == "add":
+    result = num1+num2
+  elif operation == "subtract":
+    result = num2 - num1
+  elif operation == "multiply":
+    result = num1 * num2
+  elif operation == "divide":
+    if num2 == 0:
+      result = "Null"
+    else:
+      result = num1/num2
+  return result
+
+
+
