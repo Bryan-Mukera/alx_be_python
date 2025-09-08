696e1478cb0fc419d1713c21a83667f379b3035b
Bryan Mukera <bryanmukera4@gmail.com>
2025-09-08 14:05:42 +0300

    Covered for invalid operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,15 +1,17 @@
 def perform_operation(num1, num2, operation):
   if operation == "add":
-    result = num1+num2
+    result = num1 + num2
   elif operation == "subtract":
-    result = num2 - num1
+    result = num1 - num2
   elif operation == "multiply":
     result = num1 * num2
   elif operation == "divide":
     if num2 == 0:
-      result = "Null"
+      result = "Error: Division by zero is not allowed"
     else:
       result = num1/num2
+  else:
+    result = "Error: Invalid operation"
   return result
 
 
