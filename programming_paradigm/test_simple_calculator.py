71e82c8a86c253096e46eee212e4e5c54f034d3e
Bryan Mukera <bryanmukera4@gmail.com>
2025-10-13 16:10:25 +0300

    Further commit

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -13,8 +13,7 @@ class TestSimpleCalculator(unittest.TestCase):
 
   def test_subtration(self):
     self.assertEqual(self.calc.subtract(3,2), 1)
-    self.assertEqual(self.calc.subtract(1,-1), 2)
-
+    
   def test_multiply(self):
     self.assertEqual(self.calc.multiply(3,2), 6)
     self.assertEqual(self.calc.multiply(1,3), 3)
